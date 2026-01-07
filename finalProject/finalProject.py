483f7aca4de111deda50ef6b093ae6f843853eb1
Gus Clingroth <89224907+guscling@users.noreply.github.com>
2026-01-06 21:22:23 -0600

    Update finalProject.py

--- finalProject/finalProject.py
+++ finalProject/finalProject.py
@@ -1,41 +1,4 @@
-#Name: Gus Clingroth #ID: asclingr
-#READ ME: This program is designed to, given a series of user arguments, provide a list of cities that would be viable for an MLB expansion team
-#With an MLB expansion confirmed to be coming within the next few years, there is a need to locate the most viable cities.
-    #This program provides a tool to broadly locate ideal expansion sites based on population, climate, and proximity to existing markets.
-    #This can help narrow down which cities may be viable for an MLB team.
-
-#To run this program, enter 5 user arguments in the following order:
-    #1. The minimum distance the selected cities can be from an existing MLB stadium
-    #2. The minimum population of the selected Combined Statistical Areas (CSA) NOTE: Once printed, the CSA names will be shortened to the major city within the CSA fo  ease of understanding
-    #3. The highest the average maximum temperature can be during the MLB season in the selected city (in Fahrenheit)
-    #4. The lowest the average minimum temperature can be during the MLB season in the selected city (in Fahrenheit)
-    #5. The maximum monthly precipitation during the MLB season in the selected city (in inches)
-#The program will not run properly if these five arguments are not given in this order. If fewer than 5 arguments are provided, the script will prompt the user to input the requires arguments in the console.
-#Example input: 90 2000000 90 45 6
-
-#The data necessary to run this program is zipped with this Python file.
-#The program should run correctly if it is not moved from the folder it is zipped in. The directories should be structured as follows:
-#Zipped_Folder/
-    #finalProject.py
-    #originalData/
-        #tl_2023_us_csa.shp (and auxiliary files)
-        #usa_csa_pop.csv
-        #6 tif rasters beginning with tmax_
-        #6 tif rasters beginning with tmin_
-        #12 tif rasters beginning with pre_
-        #Major_League_Baseball_Stadiums.shp (and auxiliary files)
-
-###Information about the data:
-    #The CSA boundaries are from 2023 and come from the US Census Bureau
-    #The CSA population table is from 2024 and comes from the US Census Bureau
-    #The maximum temperature, minimum temperature, and precipitation rasters all come from WorldClim.
-        #There are six average maximum temperature rasters for the months June, July, and August of 2020 and 2021. These represent the hottest months of two recent MLB seasons
-            #The raster data is in Celsius, but user inputs and the displayed information are converted to Fahrenheit
-        #There are six average minimum temperature rasters for the months April, May, and September of 2020 and 2021. These represent the coldest months of two recent MLB seasons
-            # The raster data is in Celsius, but user inputs and the displayed information are converted to Fahrenheit
-#There are 12 average monthly precipitation rasters for the months April through September of 2020 and 2021.
-            # The raster data is in millimeters, but user inputs and the displayed information are converted to inches
-#The MLB Stadiums Shapefile is from ESRI ArcGIS Hub and is accurate as of the 2025 MLB season
+####View README.md for an in-depth explanation of the script and it's data
 
 import arcpy, os, sys
 
